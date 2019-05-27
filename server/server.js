05d64e5ffb1091ae16f7c5dbe5b2ceb9fdc034e6
F7587007 <daniel_blazquez_gil@carrefour.com>
2019-05-27 10:31:44 +0200

    RestServer -usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,37 +12,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'))
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-});
+mongoose.connect(process.env.URLDB,
 
-app.post('/usuario', function(req, res) {
+    { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-});
+        if (err) throw err;
+        console.log('Base de datos Online');
+    });
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando el puerto: `, 3000);
-})
+});
